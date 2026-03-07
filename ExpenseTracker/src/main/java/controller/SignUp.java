1f0a116e5e18e4b6df19eb7a9d5de391314554bf
testingkarthi000 <testingkarthi000>
2026-03-07 17:21:25 +0530

    created login servlet , signup servlet , create mapping in web.xml

---
+++ ExpenseTracker/src/main/java/controller/SignUp.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUp extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().print("signup");
+	}
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().print("signup");
+	}
+}
