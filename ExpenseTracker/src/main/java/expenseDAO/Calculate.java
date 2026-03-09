90d4233927bee925ce24b9c2cda3a34213cdda96
karthikeyan52468 <karthikeyan60780@gmail.com>
2026-03-09 17:01:39 +0530

    added batch and trancation in calculate DAO

--- ExpenseTracker/src/main/java/expenseDAO/Calculate.java
+++ ExpenseTracker/src/main/java/expenseDAO/Calculate.java
@@ -1,8 +1,10 @@
 package expenseDAO;
 
+import java.lang.invoke.ConstantBootstraps;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Calculate {
 	
@@ -20,9 +22,11 @@ public class Calculate {
 
  public boolean calculate(double amount,int monthyear)
 	{
-		int rows=0;
-	 try {
-		 Connection con= MyConnectionDB.getConnection();
+		int len=0;
+		Connection con= null;
+		try{
+		 con= MyConnectionDB.getConnection();
+		 con.setAutoCommit(false);
 		 PreparedStatement ps = con.prepareStatement("Select amount,expense from expense where monthyear =?");
 		 ps.setInt(1, monthyear);
 		 ResultSet rs= ps.executeQuery();
@@ -37,13 +41,25 @@ public class Calculate {
 		 ps.setDouble(1, newAmount);
 		 ps.setDouble(2, newExpense);
 		 ps.setInt(3, monthyear);
-		 rows=ps.executeUpdate();
+		 ps.addBatch();
+		int rows[]=ps.executeBatch();
+		 con.commit();
+		 len=rows.length;
 		 }
 	 }
 	 catch(Exception e)
 	 {
+		 if(con!=null)
+		 {
+			 try {
+				con.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		 }
 		 e.printStackTrace();
 	 }
-	 return rows>0;
+	 return len>0;
 	}
 }
